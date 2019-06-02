1e9668f4bd5ad53bda76c075c0cb8682851ca656
Mijo Lucic <mijlucic@foi.hr>
2019-06-02 23:44:28 +0200

    Vole s

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var createError = require('http-errors');
-var express = require('express');
+var express = require('express.io');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signalRouter = require('./routes/signal');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,11 +18,22 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signal', signalRouter);
+
+app.http().io();
+
+app.io.route('ready', function (req) {
+  req.io.join(req.data);
+  app.io.room(req.data).broadcast('announce', {
+    message: 'New client in the ' + req.data + ' room.'
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
