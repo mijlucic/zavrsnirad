320c97f0d4bbc50aecc3aa78f983cccee866412e
mijlucic <mijlucic@foi.hr>
2019-08-29 22:53:10 +0200

    Com run build

--- app.js
+++ app.js
@@ -37,6 +37,18 @@ app.use('/snimanjeekrana', studentRouter);
 app.use('/users', usersRouter);
 app.use('/konzultacije', konzultacijeRouter);
 
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
